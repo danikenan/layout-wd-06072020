c5ee879ef5eaf1bcb00241c0c2fdd8a56b5ec0f3
Arenarius <34135421+arenarius@users.noreply.github.com>
2020-07-06 16:00:01 +0300

    add chart side

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -3,50 +3,84 @@ import { makeStyles } from "@material-ui/core/styles";
 import ExtraChart from "./ExtraChart";
 import InnerControls from "./InnerControls";
 import { withResizeDetector } from "react-resize-detector";
+import { Button } from "@material-ui/core";
 
 const margin = 20;
+const initialSideWidth = 100;
 const useStyles = makeStyles((theme) => ({
   contentRoot: {
     padding: margin,
     flex: 1,
     boxSizing: "border-box"
   },
+  mainChartWrapper: {
+    display: "flex",
+    flexDirection: "column"
+  },
+  mainChartInnerWrapper: {
+    display: "flex"
+  },
+  chartHeader: {
+    height: 50,
+    backgroundColor: "aliceblue",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    flexWrap: "wrap"
+  },
   mainChart: {
     backgroundColor: "mintcream",
     border: 1,
     borderStyle: "solid",
     boxSizing: "border-box"
+  },
+  chartSide: {
+    width: initialSideWidth,
+    backgroundColor: "teal"
   }
-
-  // highSomething: {
-  //   height: 2000,
-  //   backgroundColor: "green"
-  // }
 }));
 
 const Content = ({ dimensions, onAddChart, width, outsideHeight }) => {
   const classes = useStyles({ dimensions });
+  const [sideWidth, setSideWidth] = useState(initialSideWidth);
 
   const [arrayToAdd, setArrayToAdd] = useState([]);
+  const [mainChartWidth, setMainChartWidth] = useState(width - sideWidth);
 
+  console.log(mainChartWidth);
   useEffect(() => {
     onAddChart();
   }, [arrayToAdd, onAddChart]);
 
+  useEffect(() => {
+    setMainChartWidth(width - sideWidth);
+  }, [width, sideWidth]);
+
   return (
     <div className={classes.contentRoot}>
-      <div
-        className={classes.mainChart}
-        style={{
-          width: width,
-          height: outsideHeight - margin * 2 - 1
-        }}
-      >
-        <InnerControls
-          onChangeArray={(newArray) => {
-            setArrayToAdd(newArray);
-          }}
-        />
+      <div className={classes.mainChartWrapper}>
+        <div className={classes.chartHeader}>
+          chart header
+          <Button color="primary" variant="contained">
+            toggle side
+          </Button>
+        </div>
+        <div className={classes.mainChartInnerWrapper}>
+          <div
+            className={classes.mainChart}
+            style={{
+              width: mainChartWidth,
+              height: outsideHeight - margin * 2 - 50
+            }}
+          >
+            <InnerControls
+              onChangeArray={(newArray) => {
+                setArrayToAdd(newArray);
+              }}
+            />
+          </div>
+          <div className={classes.chartSide} />
+        </div>
       </div>
       {arrayToAdd.map(() => (
         <ExtraChart />
--- src/Components/ExtraChart.js
+++ src/Components/ExtraChart.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    backgroundColor: "teal",
+    backgroundColor: "slategray",
     height: 200,
     flex: 1,
     marginTop: 30
